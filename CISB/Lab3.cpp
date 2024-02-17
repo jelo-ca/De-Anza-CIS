3a3ec7600502352549a59d451cc86cb2810b1029
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2024-02-16 21:31:20 -0800

    Added Copy constructor and a kilotopound friend function.

---
+++ CISB/Lab3.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Cargo
+{
+private:
+	string uld;
+	string abbr;
+	string uldid;
+	int air;
+	double wt;
+	string dest;
+
+public:
+	//Default constructor
+	Cargo();
+
+	//Full constructor
+	Cargo(const string uld, const string abbr, const string uldid, const int air, const double wt, const string dest);
+
+	//Destructor
+	~Cargo();
+
+	//Mutators
+	void setuld();
+	void setabbr();
+	void setuldid();
+	void setair();
+	void setwt();
+	void setdest();
+
+	//Accessors
+	string getuld() const;
+	string getabbr() const;
+	string getuldid() const;
+	int getair() const;
+	double getwt() const;
+	string getdest() const;
+
+	//Kilo to pound function
+	friend double kilotopound(double kilograms);
+
+	void AssignCargo(Cargo&);
+	void DeclareCargo(Cargo&);
+};
+
+//kg to lb function
+double kilotopound(double kilograms) {
+	return kilograms * 2.2;
+}
+
+
+Cargo::Cargo() {}
+
+Cargo::Cargo(const string uld, const string abbr, const string uldid, const int air, const double wt, const string destination)
+{
+	this->uld = uld;
+	this->abbr = abbr;
+	this->uldid = uldid;
+	this->air = air;
+	this->wt = wt;
+	this->dest = dest;
+}
+
+Cargo::~Cargo() {
+	cout << "Destructor Called" << endl;
+}
+
+//MUTATORS
+void Cargo::setuld() {
+	cout << "Please input Unit Type: ";
+	cin >> this->uld;
+}
+
+void Cargo::setabbr() {
+	cout << "Unit Abbreviation: ";
+	cin >> this->abbr;
+}
+
+void Cargo::setuldid() {
+	cout << "Unit ID: ";
+	cin >> this->uldid;
+}
+
+void Cargo::setair() {
+	cout << "Aircraft: ";
+	cin >> this->air;
+}
+
+void Cargo::setwt() {
+	string unit;
+
+	cout << "Weight: ";
+	cin >> this->wt;
+	cout << "(kg/lb)?: ";
+	cin >> unit;
+	if (unit == "kg") {
+		this->wt = this->wt * 2.2;
+	}
+}
+
+void Cargo::setdest() {
+	cout << "Destination: ";
+	cin >> this->dest;
+}
+
+//ACCESSORS
+string Cargo::getuld() const {
+	return uld;
+}
+
+string Cargo::getabbr() const {
+	return abbr;
+}
+
+string Cargo::getuldid() const {
+	return uldid;
+}
+
+int Cargo::getair() const {
+	return air;
+}
+
+double Cargo::getwt() const {
+	return wt;
+}
+
+string Cargo::getdest() const {
+	return dest;
+}
+
+void Cargo::AssignCargo(Cargo& cargo) {
+	cargo.setuld();
+	cargo.setabbr();
+	cargo.setuldid();
+	cargo.setair();
+	cargo.setwt();
+	cargo.setdest();
+}
+
+void Cargo::DeclareCargo(Cargo& cargo) {
+	cout << "------------------------------------------------" <<
+		"\nUnit Type: " << cargo.getuld() <<
+		"\nAbbreviation: " << cargo.getabbr() <<
+		"\nUnit ID: " << cargo.getuldid() <<
+		"\nAircraft: " << cargo.getair() <<
+		"\nWeight: " << cargo.getwt() << " lbs" <<
+		"\nDestination: " << cargo.getdest() <<
+		"\n------------------------------------------------" << endl;
+}
+
+int main() {
+
+	Cargo* sampleCargo = new Cargo(); //Create Cargo obj w default constructor
+	sampleCargo->DeclareCargo(*sampleCargo); //Output Default Constructor
+
+
+	Cargo* load = new Cargo(); //Create Cargo obj w default constructor
+	load->AssignCargo(*load); //Use mutators to input data
+
+	Cargo* copyLoad = load; //Creates COPY
+
+
+	load->DeclareCargo(*load); //Output new contents of obj
+	load->DeclareCargo(*copyLoad); //Outputs COPY
+
+	delete sampleCargo;
+	delete load;
+	delete copyLoad;
+
+	return 0;
+}
